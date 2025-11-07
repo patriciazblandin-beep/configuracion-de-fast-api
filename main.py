fab075fecb141ed4f8dff690e88e6f2ab8e13b0b
PatriciaB <patriciazblandin@gmail.com>
2025-11-06 23:54:01 -0600

    feat: actualizaciones en main.py y agregado de carpeta utils

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from typing import Union
-
 from fastapi import FastAPI
+from utils.database import execute_query_json
+import json
 
 app = FastAPI()
 
@@ -9,6 +10,23 @@ app = FastAPI()
 def read_root():
     return {"Hello": "Patri"}
 
+@app.get("/studentss")
+async def get_all_studentss():
+    sqlscript ="""
+            SELECT [id]
+            ,[firstname]
+            ,[lastname]
+            ,[idperson]
+            ,[email]
+            ,[age]
+        FROM [academics].[studentss]
+       """
+    result = await execute_query_json(sqlscript)
+    print(type(result))
+    
+    results_dict = json.loads(result)  
+
+    return results_dict
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
